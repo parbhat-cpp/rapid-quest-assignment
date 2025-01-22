c50326376cbd9f353fa7512b15f650f26b927aa2
Parbhat Sharma <prabhat29ps@gmail.com>
2025-01-22 18:39:24 +0530

    path error fix

--- server/src/routes/image.routes.ts
+++ server/src/routes/image.routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from "multer";
+import fs from 'node:fs';
 
 import { uploadImage } from '../controllers/image.controller';
 
@@ -7,6 +8,10 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+      if (!fs.existsSync('./uploads')) {
+        fs.mkdirSync('./uploads');
+      }
+
       cb(null, './uploads')
     },
     filename: function (req, file, cb) {
